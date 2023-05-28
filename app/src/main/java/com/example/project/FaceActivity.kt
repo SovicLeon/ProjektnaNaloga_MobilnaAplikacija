cd59c5b358ee6d16d4ed12df915fddd17e352e0b
ericQronaveter <eric.mrbadass@gmail.com>
2023-05-28 21:12:40 +0200

    The session cookie now gets sent, the photo is saved in the database and it's path is properly stored

--- app/src/main/java/com/example/project/FaceActivity.kt
+++ app/src/main/java/com/example/project/FaceActivity.kt
@@ -1,46 +1,49 @@
 package com.example.project
-
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.Manifest
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Base64
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.example.project.R
 import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 import java.io.ByteArrayOutputStream
 import java.io.IOException
+import android.util.Log
 
 class FaceActivity : AppCompatActivity() {
 
     companion object {
-        private const val CAMERA_PERMISSION_REQUEST_CODE = 100
-        private const val CAMERA_REQUEST_CODE = 101
-        private const val WEBSITE_URL = "http://localhost:3001/photos/"
+        private const val CAMERA_PERMISSION_CODE = 1
+        private const val CAMERA_REQUEST_CODE = 2
     }
-
-    private lateinit var captureButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_face)
 
-        captureButton = findViewById(R.id.capture_button)
-        captureButton.setOnClickListener {
-            if (hasCameraPermission()) {
-                capturePhoto()
+        val takePhotoButton: Button = findViewById(R.id.capture_photo)
+        takePhotoButton.setOnClickListener {
+            if (checkCameraPermission()) {
+                openCamera()
             } else {
                 requestCameraPermission()
             }
         }
     }
 
-    private fun hasCameraPermission(): Boolean {
+    private fun checkCameraPermission(): Boolean {
         return ContextCompat.checkSelfPermission(
             this,
             Manifest.permission.CAMERA
@@ -51,92 +54,93 @@ class FaceActivity : AppCompatActivity() {
         ActivityCompat.requestPermissions(
             this,
             arrayOf(Manifest.permission.CAMERA),
-            CAMERA_PERMISSION_REQUEST_CODE
+            CAMERA_PERMISSION_CODE
         )
     }
 
-    private fun capturePhoto() {
-        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(intent, CAMERA_REQUEST_CODE)
+    private fun openCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
     }
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<out String>,
+        permissions: Array<String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+
+        if (requestCode == CAMERA_PERMISSION_CODE) {
             if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                capturePhoto()
+                openCamera()
             } else {
                 Toast.makeText(
                     this,
-                    "Camera permission denied.",
+                    "Camera permission denied",
                     Toast.LENGTH_SHORT
                 ).show()
             }
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
-            val photo: Bitmap? = data?.extras?.get("data") as Bitmap?
-            photo?.let {
-                val encodedPhoto = encodeBitmapToBase64(photo)
-                sendPhotoToWebsite(encodedPhoto)
-            }
-        }
-    }
-
-    private fun encodeBitmapToBase64(bitmap: Bitmap): String {
+    private fun uploadPhoto(photo: Bitmap) {
         val byteArrayOutputStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, byteArrayOutputStream)
-        val byteArray = byteArrayOutputStream.toByteArray()
-        return Base64.encodeToString(byteArray, Base64.DEFAULT)
-    }
+        val sessionCookie: String? = retrieveSessionCookie()
+        Log.d("Cookie", "$sessionCookie")
+        photo.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream)
+        val requestBody: RequestBody = MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+            .addFormDataPart(
+                "image",
+                "photo.jpg",
+                byteArrayOutputStream.toByteArray().toRequestBody("image/jpeg".toMediaTypeOrNull())
+            )
+            .build()
 
-    private fun sendPhotoToWebsite(encodedPhoto: String) {
-        val client = OkHttpClient()
-        val requestBody = FormBody.Builder()
-            .add("photo", encodedPhoto)
-            .build()
         val request = Request.Builder()
-            .url(WEBSITE_URL)
+            .url("http://192.168.1.6:3001/photos/")
             .post(requestBody)
+            .header("Cookie", sessionCookie ?: "")
             .build()
+
+        val client = OkHttpClient()
 
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
-                runOnUiThread {
-                    Toast.makeText(
-                        this@FaceActivity,
-                        "Failed to send photo.",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+                // Handle the failure case
+                e.printStackTrace()
             }
 
             override fun onResponse(call: Call, response: Response) {
+                // Handle the response
                 if (response.isSuccessful) {
-                    runOnUiThread {
-                        Toast.makeText(
-                            this@FaceActivity,
-                            "Photo sent successfully.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                    // Photo upload successful
+                    // Handle the response here
                 } else {
-                    runOnUiThread {
-                        Toast.makeText(
-                            this@FaceActivity,
-                            "Failed to send photo.",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                    // Photo upload failed
+                    // Handle the response here
                 }
             }
         })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val photo: Bitmap? = data?.extras?.get("data") as? Bitmap
+
+            photo?.let {
+                uploadPhoto(it)
+            }
+        }
+        val intent = Intent(this@FaceActivity, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    private fun retrieveSessionCookie(): String? {
+        val sharedPreferences: SharedPreferences = getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+        return sharedPreferences.getString("sessionCookie", null)
     }
 }
